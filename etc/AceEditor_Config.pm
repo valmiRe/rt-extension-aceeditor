adfd70cf31b9d8dabbe16eea3fd24ca24e7041de
remi <mirebob@gmail.com>
2014-12-10 17:42:32 +0100

    init repo

---
+++ etc/AceEditor_Config.pm
@@ -0,0 +1,9 @@
+Set($AceEditorScripsEnable,1);
+Set($AceEditorTemplatesEnable,0);
+Set($AceEditorWidth,'700px');
+Set($AceEditorMinLines,'5');
+Set($AceEditorMaxLines,'100');
+Set($AceEditorTheme,'monokai');
+Set($AceEditorScripsMode,'perl');
+Set($AceEditorTemplatesMode,'html');
+1;
---
+++ lib/RT/Extension/AceEditor.pm
@@ -0,0 +1,6 @@
+use strict;
+use warnings;
+
+package RT::Extension::AceEditor;
+
+our $VERSION = '0.01';
